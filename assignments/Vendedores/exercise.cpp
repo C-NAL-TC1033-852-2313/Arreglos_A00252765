2bdba5a01a7475a417ccbc0e22e69caea151e2e1
fabyuribe <78222449+fabyuribe@users.noreply.github.com>
2023-11-27 22:29:28 +0000

    hecho en clase

--- assignments/Vendedores/exercise.cpp
+++ assignments/Vendedores/exercise.cpp
@@ -7,8 +7,13 @@ using namespace std;
 int buscaDepartamento(Depto listaDept[], int cantDept, int idDeptoABuscar) {
     /*Función que busca un id de un departamento en el arreglo de departamentos
     si lo encuentra que regrese la posición del arreglo donde está, si no regresa -1*/
-
-
+    for (int cont = 0; cont < cantDept; cont++) {
+        if (listaDept[cont].getIdDep() == idDeptoABuscar) {
+            return cont; // ya encontré la posición del departamento
+        }
+    }
+    // si sale del for, quiere decir que no existe porque si exisiera se hubiera regresado adentro del for
+    cout << "Departamento incorrecto" << endl;
     return -1;
 }
 
@@ -19,34 +24,33 @@ string nombreDepartamento(Depto listaDept[], int cantDept, int idDeptoABuscar) {
     return "";
 }
 
-void cargaDatosDepartamentos(Depto listaDeptos[], int &cantDeptos) {
+void cargaDatosDepartamentos(Depto listaDeptos[], int& cantDeptos) {
     /*Carga datos del archivo Deptos.txt en listaDeptos y cantDeptos lo actualiza
       con el número de departamentos cargados del archivo*/
     int numDepartamento;
     string nombreDepartamento;
-    cantDeptos = 0;   // al inicio no hay ningún departamento en el arreglo listaDeptos
-    ifstream archivo;    // para leer datos de un archivo (input file stream)
+    ifstream archivo;
     archivo.open("Deptos.txt");
     if(archivo.is_open()){
-        //Leo mientras haya datos que leer
-        while ( archivo >> numDepartamento >> nombreDepartamento ) {
+        while(archivo >> numDepartamento >> nombreDepartamento){
             listaDeptos[cantDeptos].setIdDep(numDepartamento);
             listaDeptos[cantDeptos].setNombre(nombreDepartamento);
             cantDeptos++;
         }
-        archivo.close();
     }
     else{
-        cout << "No se pudo abrir el archivo" << endl;
+        cout << "El archivo no se pudo abrir" << endl;
     }
-
 }
 
 void muestraDepartamentos(Depto listaDept[], int cantDep) {
     /*Muestra la lista de departamentos*/
     cout << "Departamentos de la tienda" << endl;
-    
+    for (int cont = 0; cont < cantDep; cont++) {
+        listaDept[cont].imprime();
+    }
 }
+    
 
 void cargaDatosVendedores(Vendedor listaVend[], int &cantVend, Depto listaDep[], int cantDep) {
     /* Función que regresa la listaVend y la cantVend con datos recibe el arreglo vacío
@@ -55,16 +59,61 @@ void cargaDatosVendedores(Vendedor listaVend[], int &cantVend, Depto listaDep[],
     string nom;
     double vent;
     int num;
-    
+    cout << "Cuantos vendedores? ";
+    cin >> cantVend;
+    for (int cont = 0; cont < cantVend; cont++) {
+        cout << "Teclea el nombre ";
+        cin.ignore();
+        getline(cin,nom);
+        cout << "Teclea las ventas ";
+        cin >> vent;
+        do{
+            cout << "ID del departamento: ";
+            cin >> idDep;
+            num = buscaDepartamento(listaDep, cantDep, idDep);
+        }while(num == -1);
+        listaVend[cont].setNombre(nom);
+        listaVend[cont].setVentas(vent);
+        listaVend[cont].setDepartamento(listaDep[num]);
+    }
+}
+
+void cargaVendedoresArchivo(Vendedor listaVend[], int &cantVend, Depto listaDep[], int cantDep){
+    string nom;
+    int id, pos;
+    double vent;
+    ifstream archivo;
+    archivo.open("vendedores.txt");
+    if(archivo.is_open()){
+        while(archivo >> nom >> vent >> id){
+            listaVend[cantVend].setNombre(nom);
+            listaVend[cantVend].setVentas(vent);
+            pos = buscaDepartamento(listaDep, cantDep, id);
+            listaVend[cantVend].setDepartamento(listaDep[pos]);
+            cantVend++;
+        }
+    }
+    else{
+        cout << "Archivo no encontrado" << endl;
+    }
+
 }
 
 void muestraVendedores(Vendedor listaVen[], int cantVen) {
     /*Muestra la lista de vendedores*/
-    
+    for (int cont = 0; cont < cantVen; cont++) {
+        listaVen[cont].imprime();
+    }
 }
 
-void vendedoresPorDepartamento(Vendedor listaVen[], int cantVend, Depto listaDep[], int cantDept, int idDeptoAConsultar) {
+
+void vendedoresPorDepartamento(Vendedor listaVen[], int cantVend, int idDeptoAConsultar) {
     /* Muestra los nombres de los vendedores de un departamento */
+    for(int i=0; i<cantVend; i++){
+        if(listaVen[i].getDepartamento().getIdDep() == idDeptoAConsultar){
+            cout << listaVen[i].getNombre() << endl;
+        }
+    }
     
 }
 
@@ -82,7 +131,8 @@ int main() {
     Depto listaDeptos[MAX]; // inicio el arreglo con Deptos default.
     int cantDeptos = 0;  // cantidad de departamentos en la lista de deptos
     cargaDatosDepartamentos(listaDeptos, cantDeptos);
-    cargaDatosVendedores(listaVendedores, cantVendedores, listaDeptos, cantDeptos);
+    //cargaDatosVendedores(listaVendedores, cantVendedores, listaDeptos, cantDeptos);
+    cargaVendedoresArchivo(listaVendedores, cantVendedores, listaDeptos, cantDeptos);
     do {
         cout << endl;
         cout << "Opciones disponibles " << endl;
@@ -108,7 +158,7 @@ int main() {
             case '3': 
                 cout << "Teclea el departamento que quieres consultar " << endl;
                 cin >> numDepto;
-                vendedoresPorDepartamento(listaVendedores, cantVendedores, listaDeptos, cantDeptos, numDepto);
+                vendedoresPorDepartamento(listaVendedores, cantVendedores, numDepto);
                 cin.ignore();
                 cout << "\nEnter para continuar...";
                 cin.get();
